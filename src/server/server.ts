3b9500aba723b805e856450b01b876d824f9ab1f
Esther Falayi <esther.falayi@andela.com>
2020-10-23 16:53:32 +0100

    (feature): create server
    - setup proxy
    - create .env.sample file
    - install dotenv
    - install http-proxy-middleware

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,16 +1,28 @@
 import express from "express";
+import { createProxyMiddleware } from 'http-proxy-middleware';
 import path from "path";
+import dotenv from 'dotenv';
 
+dotenv.config();
+
+const PORT = process.env.API_PORT || 3000;
+const HOST = process.env.API_HOST;
 const buildDir = path.join(process.cwd() + "/build");
 const app = express();
+
 app.use(express.static(buildDir));
+app.use('/api',
+  createProxyMiddleware({
+    target: HOST,
+    changeOrigin: true,
+  })
+);
 
 app.get("/*", function (req, res) {
   res.sendFile(path.join(buildDir, "index.html"));
 });
 
-const port = process.env.API_PORT || 3000;
-console.log("checking port", port);
-app.listen(port, () => {
-  console.log(`Server now listening on port: ${port}`);
+console.log("checking port", PORT);
+app.listen(PORT, () => {
+  console.log(`API now running on port: ${PORT}`);
 });
